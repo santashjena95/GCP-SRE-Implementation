4ec6fb6d631ec366c47ee14d8f1d07575696bf90
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2024-09-25 19:26:08 +0530

    Update mocha_tests.spec.js

--- synthetic_monitoring/mocha_tests.spec.js
+++ synthetic_monitoring/mocha_tests.spec.js
@@ -2,15 +2,24 @@ const axios = require('axios');
 const assert = require('assert');
 const httpsAgent = new (require('https')).Agent({rejectUnauthorized: false});
 
-describe('API Tests', () => {
-    it('RCADM Get Report  Should Return 200', async function() {
+const fs = require('fs');
+const path = require('path');
+
+// Read the configuration file
+const configPath = path.join(__dirname, 'endpoints.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+describe('Uptime Checks', () => {
+    config.endpoints.forEach(({ name, url }) => {
+    it(`should return 200 for ${name}:(${url})`, async function() {
         try {
-            const response = await axios.get('https://hello-1058717846530.us-central1.run.app', { httpsAgent });
+            const response = await axios.get(url, { httpsAgent });
             assert.equal(response.status, 200);
-                    console.log('Success Call For RCADM to get report API');
+                    console.log(`Success Call For ${name}:(${url})`);
         } catch (error) {
-                      console.log('Error and Failed to call RCADM Get Report API: ' + error.message);
-            assert.fail('Failed to call API:' + error.message);
+                      console.log(`Error and Failed to call for ${url}: ` + error.message);
+            assert.fail('Failed to Uptime Check:' + error.message);
         }
     }).timeout(60000);
+   });
 });
