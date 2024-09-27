6febd9c8bff52fdbc9b7c792951b6f171afa546e
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2024-09-27 14:58:12 +0530

    Update mocha_tests.spec.js

--- synthetic_monitoring/mocha_tests.spec.js
+++ synthetic_monitoring/mocha_tests.spec.js
@@ -4,30 +4,42 @@ const https = require('https');
 
 const fs = require('fs');
 const path = require('path');
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-// Set up a custom HTTPS agent to ignore unauthorized SSL/TLS certificates
+
 const httpsAgent = new https.Agent({rejectUnauthorized: false});
 
-// Read the endpoints configuration from a JSON file
+
 const configPath = path.join(__dirname, 'endpoints.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-// Define a test suite for checking uptime of endpoints
+const secretClient = new SecretManagerServiceClient();
+
+async function accessSecret(secretName) {
+  const [version] = await secretClient.accessSecretVersion({
+    name: secretName,
+  });
+  return version.payload.data.toString('utf8');
+}
+
 describe('Uptime Checks', () => {
-    // Iterate through each endpoint defined in the config file
+
     config.endpoints.forEach(({ name, url }) => {
-        // Define a test case for each endpoint
+
         it(`should return 200 OK for ${name}: (${url})`, async function() {
-            this.timeout(60000); // Set the timeout for this test
+            this.timeout(60000);
+            const username = await accessSecret(auth.usernameSecret);
+            const password = await accessSecret(auth.passwordSecret);
+            axiosConfig.auth = { username, password };
             try {
-                // Make an HTTP GET request to the endpoint using axios with the custom agent
-                const response = await axios.get(url, { httpsAgent });
-                // Assert that the HTTP response status is 200
+
+                const response = await axios.get(url, { httpsAgent }, axiosConfig);
+
                 assert.equal(response.status, 200);
-                console.log(`Success Call For ${name}: (${url})`); // Log success
+                console.log(`Success Call For ${name}: (${url})`);
             } catch (error) {
-                // Log the error and fail the test if an exception occurs
-                console.error(`Error and Failed call for ${url}: ` + error.message);
+
+                console.error(`Failed call for ${url}`);
                 assert.fail(`Failed Uptime Check: ${error.message}`);
             }
         });
