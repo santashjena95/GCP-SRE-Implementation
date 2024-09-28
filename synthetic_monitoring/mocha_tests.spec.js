d6783937a63e4a4cb97f1b8d412f8fc8c473a095
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2024-09-28 09:44:50 +0530

    Update mocha_tests.spec.js

--- synthetic_monitoring/mocha_tests.spec.js
+++ synthetic_monitoring/mocha_tests.spec.js
@@ -4,42 +4,34 @@ const https = require('https');
 
 const fs = require('fs');
 const path = require('path');
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-
+// Set up a custom HTTPS agent to ignore unauthorized SSL/TLS certificates
 const httpsAgent = new https.Agent({rejectUnauthorized: false});
+const filejson = process.env.FILE_JSON
 
-
-const configPath = path.join(__dirname, 'endpoints.json');
+// Read the endpoints configuration from a JSON file
+const configPath = path.join(__dirname, filejson);
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-const secretClient = new SecretManagerServiceClient();
-
-async function accessSecret(secretName) {
-  const [version] = await secretClient.accessSecretVersion({
-    name: secretName,
-  });
-  return version.payload.data.toString('utf8');
-}
-
+// Define a test suite for checking uptime of endpoints
 describe('Uptime Checks', () => {
-
-    config.endpoints.forEach(({ name, url }) => {
-
+    // Iterate through each endpoint defined in the config file
+    config.endpoints.forEach(({ name, url, expectedStatus, expectedStatusText, expectedData }) => {
+        // Define a test case for each endpoint
         it(`should return 200 OK for ${name}: (${url})`, async function() {
-            this.timeout(60000);
-            const username = await accessSecret(auth.usernameSecret);
-            const password = await accessSecret(auth.passwordSecret);
-            axiosConfig.auth = { username, password };
+            this.timeout(60000); // Set the timeout for this test
             try {
-
-                const response = await axios.get(url, { httpsAgent }, axiosConfig);
-
-                assert.equal(response.status, 200);
-                console.log(`Success Call For ${name}: (${url})`);
+                // Make an HTTP GET request to the endpoint using axios with the custom agent
+                const response = await axios.get(url, { httpsAgent });
+                const responsedata = response.data["url"];
+                // Assert that the HTTP response status is 200
+                assert.equal(response.status, expectedStatus);
+                assert.equal(response.statusText, expectedStatusText);
+                assert.equal(responsedata, expectedData);
+                console.log(`Success Call For ${name}: (${url}) response data value: ${responsedata} response status value: ${response.status} response Text: ${response.statusText}`); // Log success
             } catch (error) {
-
-                console.error(`Failed call for ${url}`);
+                // Log the error and fail the test if an exception occurs
+                console.error(`Failed Call For ${url}: ` + error.message);
                 assert.fail(`Failed Uptime Check: ${error.message}`);
             }
         });
