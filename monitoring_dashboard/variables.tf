23d2068cc9a2638c99fe6efecce2081b034837a1
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2024-10-04 11:56:55 +0530

    Create variables.tf

---
+++ monitoring_dashboard/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID of the cloud run service"
+}
+variable "environment" {
+  type        = string
+  description = "The Environment of the deployment"
+}
