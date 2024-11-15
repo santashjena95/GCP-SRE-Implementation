58a39d4c6bdca51707f96620cebeb67fd722a7de
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2024-11-15 17:59:57 +0530

    Update main_multiple_rows.py

--- BigQuery_View/main_multiple_rows.py
+++ BigQuery_View/main_multiple_rows.py
@@ -3,29 +3,32 @@ import os
 import json
 
 def query_view(data):
-    config_file = os.environ.get("FILE_JSON")
-    with open(config_file, 'r') as file:
-        config = json.load(file)
- 
-    project_id = config['settings']['project_id']
-    dataset_id = config['settings']['dataset_id']
-    view_id = config['settings']['view_id']
-    # Build a BigQuery client object.
-    client = bigquery.Client(project=project_id)
+    try:
+        config_file = os.environ.get("FILE_JSON")
+        with open(config_file, 'r') as file:
+            config = json.load(file)
+    
+        project_id = config['settings']['project_id']
+        dataset_id = config['settings']['dataset_id']
+        view_id = config['settings']['view_id']
+        client = bigquery.Client(project=project_id)
 
-    # Qualify the view with its dataset and project.
-    view_ref = f"{project_id}.{dataset_id}.{view_id}"
+        # Qualify the view with its dataset and project.
+        view_ref = f"{project_id}.{dataset_id}.{view_id}"
 
-    # Construct a full SQL query.
-    query = f"SELECT * FROM `{view_ref}`"
+        # Construct a full SQL query.
+        query = f"SELECT * FROM `{view_ref}`"
 
-    # Execute the query. Wait for the job to finish
-    query_job = client.query(query)
+        # Execute the query. Wait for the job to finish
+        query_job = client.query(query)
+    except Exception as e:
+        print(f"Config file not present: {e}")
+        return "Not Done"
 
     try:
         # Fetch the results and process them
         results = query_job.result()  # Waits for the job to complete
-        print(results)
+        
         for row in results:
             Id = int(row['Id'])
             name = row['Name']
@@ -33,6 +36,8 @@ def query_view(data):
                 print(f"CEO: {name}")
             else:
                 print(f"Normal Employee: {name}")
-        return "Query Executed Successfully..."
+        print("Query Executed Successfully...")
+        return "Done"
     except Exception as e:
         print(f"Failed to query the view: {e}")
+        return "Not Done"
