8dcf2aca890c166c3c58e45fa91d585b386ed8ae
santashjena95 <57282793+santashjena95@users.noreply.github.com>
2025-01-05 20:45:12 +0530

    Create variables.tf

---
+++ multiple_condition/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID of the cloud run service"
+}
+
+variable "email_address_notify" {
+  type        = string
+  description = "Email Id of the error message recipient"
+}
